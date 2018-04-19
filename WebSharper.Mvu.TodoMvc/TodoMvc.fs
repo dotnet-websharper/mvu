c06eea33acdb8fd3c3ed64116aa5677b8d827f75
Loic Denuziere <loic@denuziere.net>
2018-04-19 18:39:11 +0200

    Reorganize TodoMvc by component instead of by model / view / update

--- WebSharper.Mvu.TodoMvc/TodoMvc.fs
+++ WebSharper.Mvu.TodoMvc/TodoMvc.fs
@@ -10,14 +10,23 @@ open WebSharper.UI.Client
 open WebSharper.UI.Templating
 open WebSharper.Mvu
 
-/// This module defines the types of our application's model.
-module Model =
+/// Parses the index.html file and provides types to fill it with dynamic content.
+type MasterTemplate = Template<"wwwroot/index.html", ClientLoad.FromDocument>
+
+/// Our application has three URL endpoints.
+type EndPoint =
+    | [<EndPoint "/">] All
+    | [<EndPoint "/active">] Active
+    | [<EndPoint "/completed">] Completed
+
+/// This module defines the model, the update and the view for a single entry.
+module Entry =
 
     /// The unique identifier of a Todo entry.
     type Key = int
 
     /// The model for a Todo entry.
-    type Entry =
+    type Model =
         {
             Id : Key
             Task : string
@@ -25,29 +34,87 @@ module Model =
             Editing : option<string>
         }
 
-        static member Key (e: Entry) = e.Id
+    let KeyOf (e: Model) = e.Id
 
-        static member New (key: Key) (task: string) =
-            {
-                Id = key
-                Task = task
-                IsCompleted = false
-                Editing = None
-            }
+    let New (key: Key) (task: string) =
+        {
+            Id = key
+            Task = task
+            IsCompleted = false
+            Editing = None
+        }
 
-    /// Our application has three URL endpoints.
-    type EndPoint =
-        | [<EndPoint "/">] All
-        | [<EndPoint "/active">] Active
-        | [<EndPoint "/completed">] Completed
+    [<NamedUnionCases "type">]
+    type Message =
+        | Remove
+        | StartEdit
+        | Edit of text: string
+        | CommitEdit
+        | CancelEdit
+        | SetCompleted of completed: bool
+
+    /// Defines how a given Todo entry is updated based on a message.
+    /// Returns Some to update the entry, or None to delete it.
+    let Update (msg: Message) (e: Model) : option<Model> =
+        match msg with
+        | Remove ->
+            None
+        | StartEdit ->
+            Some { e with Editing = e.Editing |> Option.orElse (Some e.Task) }
+        | Edit value ->
+            Some { e with Editing = Some value }
+        | CommitEdit ->
+            Some { e with
+                    Task = e.Editing |> Option.defaultValue e.Task
+                    Editing = None }
+        | CancelEdit ->
+            Some { e with Editing = None }
+        | SetCompleted value ->
+            Some { e with IsCompleted = value }
+
+    /// Render a given Todo entry.
+    let Render (dispatch: Message -> unit) (endpoint: View<EndPoint>) (entry: View<Model>) =
+        MasterTemplate.Entry()
+            .Label(text entry.V.Task)
+            .CssAttrs(
+                Attr.ClassPred "completed" entry.V.IsCompleted,
+                Attr.ClassPred "editing" entry.V.Editing.IsSome,
+                Attr.ClassPred "hidden" (
+                    match endpoint.V, entry.V.IsCompleted with
+                    | EndPoint.Completed, false -> true
+                    | EndPoint.Active, true -> true
+                    | _ -> false
+                )
+            )
+            .EditingTask(
+                V(entry.V.Editing |> Option.defaultValue ""),
+                fun text -> dispatch (Message.Edit text)
+            )
+            .EditBlur(fun _ -> dispatch Message.CommitEdit)
+            .EditKeyup(fun e ->
+                match e.Event.Key with
+                | "Enter" -> dispatch Message.CommitEdit
+                | "Escape" -> dispatch Message.CancelEdit
+                | _ -> ()
+            )
+            .IsCompleted(
+                V entry.V.IsCompleted,
+                fun x -> dispatch (Message.SetCompleted x)
+            )
+            .Remove(fun _ -> dispatch Message.Remove)
+            .StartEdit(fun _ -> dispatch Message.StartEdit)
+            .Doc()
+
+/// This module defines the model, the update and the view for a full todo list.
+module TodoList =    
 
     /// The model for the full TodoList application.
-    type TodoList =
+    type Model =
         {
             EndPoint : EndPoint
             NewTask : string
-            Entries : list<Entry>
-            NextKey : Key
+            Entries : list<Entry.Model>
+            NextKey : Entry.Key
         }
 
         static member Empty =
@@ -58,146 +125,75 @@ module Model =
                 NextKey = 0
             }
 
-
-/// This module defines the updates that can be applied to the application's model.
-module Update =
-
-    /// Updates for a specific Todo entry.
-    module Entry =
-
-        [<NamedUnionCases "type">]
-        type Message =
-            | Remove
-            | StartEdit
-            | Edit of text: string
-            | CommitEdit
-            | CancelEdit
-            | SetCompleted of completed: bool
-
-        /// Defines how a given Todo entry is updated based on a message.
-        /// Returns Some to update the entry, or None to delete it.
-        let Update (msg: Message) (e: Model.Entry) : option<Model.Entry> =
-            match msg with
-            | Remove ->
-                None
-            | StartEdit ->
-                Some { e with Editing = e.Editing |> Option.orElse (Some e.Task) }
-            | Edit value ->
-                Some { e with Editing = Some value }
-            | CommitEdit ->
-                Some { e with
-                        Task = e.Editing |> Option.defaultValue e.Task
-                        Editing = None }
-            | CancelEdit ->
-                Some { e with Editing = None }
-            | SetCompleted value ->
-                Some { e with IsCompleted = value }
-
     [<NamedUnionCases "type">]
     type Message =
         | EditNewTask of text: string
         | AddEntry
         | ClearCompleted
         | SetAllCompleted of completed: bool
-        | EntryMessage of key: Model.Key * message: Entry.Message
+        | EntryMessage of key: Entry.Key * message: Entry.Message
 
     /// Defines how the Todo list is updated based on a message.
-    let TodoList (msg: Message) (model: Model.TodoList) =
+    let Update (msg: Message) (model: Model) =
         match msg with
         | EditNewTask value ->
             { model with NewTask = value }
         | AddEntry ->
             { model with
                 NewTask = ""
-                Entries = model.Entries @ [Model.Entry.New model.NextKey model.NewTask]
+                Entries = model.Entries @ [Entry.New model.NextKey model.NewTask]
                 NextKey = model.NextKey + 1 }
         | ClearCompleted ->
             { model with Entries = List.filter (fun e -> not e.IsCompleted) model.Entries }
         | SetAllCompleted c ->
             { model with Entries = List.map (fun e -> { e with IsCompleted = c }) model.Entries }
         | EntryMessage (key, msg) ->
-            let updateEntry (e: Model.Entry) =
-                if e.Id = key then Entry.Update msg e else Some e
+            let updateEntry (e: Entry.Model) =
+                if Entry.KeyOf e = key then Entry.Update msg e else Some e
             { model with Entries = List.choose updateEntry model.Entries }
 
-
-/// This module defines the rendering of our application.
-module Render =
-
-    /// Parses the index.html file and provides types to fill it with dynamic content.
-    type MasterTemplate = Template<"wwwroot/index.html", ClientLoad.FromDocument>
-
-    /// Render a given Todo entry.
-    let Entry (dispatch: Update.Entry.Message -> unit) (endpoint: View<Model.EndPoint>) (entry: View<Model.Entry>) =
-        MasterTemplate.Entry()
-            .Label(text entry.V.Task)
-            .CssAttrs(
-                Attr.ClassPred "completed" entry.V.IsCompleted,
-                Attr.ClassPred "editing" entry.V.Editing.IsSome,
-                Attr.ClassPred "hidden" (
-                    match endpoint.V, entry.V.IsCompleted with
-                    | Model.Completed, false -> true
-                    | Model.Active, true -> true
-                    | _ -> false
+    /// Render the whole application.
+    let Render (dispatch: Message -> unit) (state: View<Model>) =
+        let countNotCompleted =
+            V(state.V.Entries
+                |> List.filter (fun e -> not e.IsCompleted)
+                |> List.length)
+        MasterTemplate()
+            .Entries(
+                V(state.V.Entries).DocSeqCached(Entry.KeyOf, fun key (entry: View<Entry.Model>) ->
+                    let entryDispatch msg = dispatch (EntryMessage (key, msg))
+                    Entry.Render entryDispatch (V state.V.EndPoint) entry
                 )
             )
-            .EditingTask(
-                V(entry.V.Editing |> Option.defaultValue ""),
-                fun text -> dispatch (Update.Entry.Edit text)
-            )
-            .EditBlur(fun _ -> dispatch Update.Entry.CommitEdit)
-            .EditKeyup(fun e ->
-                match e.Event.Key with
-                | "Enter" -> dispatch Update.Entry.CommitEdit
-                | "Escape" -> dispatch Update.Entry.CancelEdit
-                | _ -> ()
-            )
+            .ClearCompleted(fun _ -> dispatch Message.ClearCompleted)
             .IsCompleted(
-                V entry.V.IsCompleted,
-                fun x -> dispatch (Update.Entry.SetCompleted x)
-            )
-            .Remove(fun _ -> dispatch Update.Entry.Remove)
-            .StartEdit(fun _ -> dispatch Update.Entry.StartEdit)
-            .Doc()
-
-    /// Render the whole application.
-    let TodoList (dispatch: Update.Message -> unit) (state: View<Model.TodoList>) =
-        MasterTemplate()
-            .Entries(V(state.V.Entries).DocSeqCached(Model.Entry.Key, fun key entry ->
-                let entryDispatch msg = dispatch (Update.EntryMessage (key, msg))
-                Entry entryDispatch (V state.V.EndPoint) entry
-            ))
-            .ClearCompleted(fun _ -> dispatch Update.ClearCompleted)
-            .IsCompleted(
-                V(match state.V.Entries with
-                    | [] -> false
-                    | l -> l |> List.forall (fun e -> e.IsCompleted)),
-                fun c -> dispatch (Update.SetAllCompleted c)
+                V(countNotCompleted.V = 0),
+                fun c -> dispatch (Message.SetAllCompleted c)
             )
             .Task(
                 V state.V.NewTask,
-                fun text -> dispatch (Update.EditNewTask text)
+                fun text -> dispatch (Message.EditNewTask text)
             )
             .Edit(fun e ->
                 if e.Event.Key = "Enter" then
-                    dispatch Update.AddEntry
+                    dispatch Message.AddEntry
                     e.Event.PreventDefault()
             )
             .ItemsLeft(
-                V(match List.length state.V.Entries with
+                V(match countNotCompleted.V with
                     | 1 -> "1 item left"
                     | n -> string n + " items left")
             )
-            .CssFilterAll(Attr.ClassPred "selected" (state.V.EndPoint = Model.All))
-            .CssFilterActive(Attr.ClassPred "selected" (state.V.EndPoint = Model.Active))
-            .CssFilterCompleted(Attr.ClassPred "selected" (state.V.EndPoint = Model.Completed))
+            .CssFilterAll(Attr.ClassPred "selected" (state.V.EndPoint = EndPoint.All))
+            .CssFilterActive(Attr.ClassPred "selected" (state.V.EndPoint = EndPoint.Active))
+            .CssFilterCompleted(Attr.ClassPred "selected" (state.V.EndPoint = EndPoint.Completed))
             .Bind()
 
 /// The entry point of our application, called on page load.
 [<SPAEntryPoint>]
 let Main () =
-    let app = App.CreateSimple Model.TodoList.Empty Update.TodoList Render.TodoList
-    App.WithRouting (Router.Infer()) (fun (model: Model.TodoList) -> model.EndPoint) app
+    let app = App.CreateSimple TodoList.Model.Empty TodoList.Update TodoList.Render
+    App.WithRouting (Router.Infer()) (fun (model: TodoList.Model) -> model.EndPoint) app
     |> App.WithLocalStorage "todolist"
     |> App.WithRemoteDev (RemoteDev.Options(hostname = "localhost", port = 8000))
     |> App.Run
