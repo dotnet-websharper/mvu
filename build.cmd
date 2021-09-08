91badfa453710787362fe54c69757a252e1f7136
jooseppi12 <urijozsef@europe.com>
2021-09-08 17:30:34 +0200

    Update build.cmd and build.sh

--- build.cmd
+++ build.cmd
@@ -1,10 +1,9 @@
 @echo off
 setlocal
-    
+
 dotnet tool restore
-dotnet paket restore
-dotnet restore
+
+dotnet paket update -g wsbuild --no-install
 if errorlevel 1 exit /b %errorlevel%
-    
-call paket-files\wsbuild\github.com\dotnet-websharper\build-script\update.cmd
+
 call paket-files\wsbuild\github.com\dotnet-websharper\build-script\build.cmd %*
--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-    
+
 dotnet tool restore
-dotnet paket restore
-dotnet restore
-    
-. paket-files/wsbuild/github.com/dotnet-websharper/build-script/update.sh
+
+dotnet paket update -g wsbuild --no-install
+
 . paket-files/wsbuild/github.com/dotnet-websharper/build-script/build.sh "$@"
