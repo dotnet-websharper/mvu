65f3371fc9ddef868560ad32e940fb42be1ef70c
Jooseppi12 <urijozsef@europe.com>
2024-07-05 14:34:58 +0200

    Prep for net8

--- build.fsx
+++ build.fsx
@@ -1,4 +1,3 @@
-#if INTERACTIVE
 #r "nuget: FAKE.Core"
 #r "nuget: Fake.Core.Target"
 #r "nuget: Fake.IO.FileSystem"
@@ -6,30 +5,15 @@
 #r "nuget: Fake.DotNet.Cli"
 #r "nuget: Fake.DotNet.AssemblyInfoFile"
 #r "nuget: Fake.DotNet.Paket"
-#r "nuget: Paket.Core"
-#else
-#r "paket:
-nuget FSharp.Core 5.0.0
-nuget FAKE.Core
-nuget Fake.Core.Target
-nuget Fake.IO.FileSystem
-nuget Fake.Tools.Git
-nuget Fake.DotNet.Cli
-nuget Fake.DotNet.AssemblyInfoFile
-nuget Fake.DotNet.Paket
-nuget Paket.Core prerelease //"
-#endif
+#r "nuget: Paket.Core, 8.1.0-alpha004"
+
+open Fake.Core
+let execContext = Context.FakeExecutionContext.Create false "build.fsx" []
+Context.setExecutionContext (Context.RuntimeContext.Fake execContext)
 
 #load "paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.fsx"
 #r "System.Xml.Linq"
 
-// Only reference these packages from editors/non fake-cli tools
-#if !FAKE
-    // To have proper language service in the editor
-    #r "netstandard"
-    // To help FAKE related IntelliSense in editor
-    #load "./.fake/build.fsx/intellisense_lazy.fsx"
-#endif
 
 open WebSharper.Fake
 
