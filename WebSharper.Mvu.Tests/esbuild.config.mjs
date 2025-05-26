99ee2f5fd101098ff74b5d2c5486a58fdc39a279
András Jankó <andras.janko@gmail.com>
2025-05-26 16:36:35 +0200

    update test projects to use esbuild

---
+++ WebSharper.Mvu.Tests/esbuild.config.mjs
@@ -0,0 +1,23 @@
+﻿import { existsSync, cpSync, readdirSync } from 'fs'
+import { build } from 'esbuild'
+
+const files = readdirSync('./build/');
+
+cpSync('./build/WebSharper.Mvu.Tests.css', './wwwroot/Scripts/WebSharper.Mvu.Tests.css', { force: true });
+
+files.forEach(file => {
+  if (file.endsWith('.js')) {
+    var options =
+    {
+      entryPoints: ['./build/' + file],
+      bundle: true,
+      minify: true,
+      format: 'iife',
+      outfile: 'wwwroot/Scripts/' + file,
+      globalName: 'wsbundle'
+    };
+
+    console.log("Bundling:", file);
+    build(options);
+  }
+});
---
+++ WebSharper.Mvu.Tests/wwwroot/Scripts/WebSharper.Mvu.Tests.head.js
@@ -0,0 +1,1 @@
+var wsbundle=(()=>{document.write("");})();
